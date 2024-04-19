a52503ab182a3c8f706b88b35e5d9bfa8be47343
Hintonein <48764636+Hintonein@users.noreply.github.com>
2024-04-19 19:36:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from openxlab.model import download
 base_path = './Horo_1.8B_SFT'
 os.system(f'git clone https://code.openxlab.org.cn/Hintonein/Horo_1.8B_SFT.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
+os.system('ls')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
